18ba4d142fe8ad8dde5b18b518803ed6bf6a5a89
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-04-17 12:27:08 +0100

    solved algo challenge

---
+++ app.js
@@ -0,0 +1,55 @@
+const divisible = (number) => {
+   if (typeof number === 'number'){
+    const newArr = []; 
+    for (let i = 1; i <= number; i++) {
+      if (i % 2  !== 0 && i % 3 !== 0 && i % 5  !== 0  ){
+        newArr.push(i);
+      }
+      
+      if (i % 2 === 0) {
+        newArr.push('yu');
+         
+        if (i % 5 === 0) {
+          newArr[newArr.length - 1] = 'yu-oh';
+        }
+        
+        if (i % 3 === 0)  {
+          newArr[newArr.length - 1] = 'yu-gi';
+          
+           if (i % 5 === 0) {
+            newArr[newArr.length - 1] = 'yu-gi-oh';
+          }
+        }
+        
+      
+      }
+      
+      if (i % 2 == 0 && i % 3 === 0) {
+          
+      } else {
+        if (i % 3 === 0) {
+          newArr.push('gi');
+            if (i % 5 === 0) {
+            newArr[newArr.length - 1] = 'gi-oh';
+          }
+        }
+      }
+      
+      if (i % 2 == 0 && i % 5 === 0) {
+          
+      } else {
+        if (i % 3 == 0 && i % 5 === 0){
+          
+        } else{
+          if (i % 5 === 0) {
+            newArr.push('oh');
+          } 
+        }
+      }
+   }
+    return newArr;
+   }
+ }
+ divisible(100);
+
+ divisible(10);
